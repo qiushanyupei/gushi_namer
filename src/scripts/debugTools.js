9a359e76654b45d3c555a6308572a009b7bc1f50
qiushanyupei <136561717+qiushanyupei@users.noreply.github.com>
2025-06-15 22:10:17 +0800

    Update debugTools.js

--- src/scripts/debugTools.js
+++ src/scripts/debugTools.js
@@ -1,3 +1,4 @@
 import { debugMode } from './config';
 
 export const log = debugMode ? console.log.bind(console) : () => { };
+//js中bind是内置函数，用于对某个函数绑定this值，保证不出现覆盖定义等一场情况，导致报错，实际上就是定义了console.log函数的简写
